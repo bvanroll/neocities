bd7727b41f75c5dac25e673469444f8151ea34cf
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 21:46:45 +0200

    first test action

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+t='['
 for entry in notes/*
 do 
   echo $entry
-  test=`git log --follow --format=%ad --date=format:'%Y-%m-%d' -- $entry`
-  echo $test
+  filename=$(basename "$entry" .md)
+  #filename="${entry%}"
+  timestamp=`git log --follow --format=%ad --date=format:'%Y-%m-%d' -- $entry`
+  t+="{\"source\"=\"$entry\",\"dest\"=\"$filename\",\"time\"=\"$timestamp\"},"
 done
+echo "value=${t%?}]"
+echo "value=${t%?}]" >> $GITHUB_OUTPUT
 
