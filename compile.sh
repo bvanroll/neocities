ac31c08bcaea9e809fc7c85269b2a1b1b240b21d
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 22:09:46 +0200

    empty

--- compile.sh
+++ compile.sh
@@ -2,12 +2,9 @@
 t='['
 for entry in notes/*
 do 
-  echo $entry
   filename=$(basename "$entry" .md)
-  #filename="${entry%}"
   timestamp=`git log --follow --format=%ad --date=format:'%Y-%m-%d' -- $entry`
   t+="{\"source\"=\"$entry\",\"dest\"=\"$filename\",\"time\"=\"$timestamp\"},"
 done
 echo "value=${t%?}]"
-echo "value=${t%?}]" >> $GITHUB_OUTPUT
 
