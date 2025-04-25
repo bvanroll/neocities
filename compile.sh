9fdfea75426fc24ef33599b9672a78c7884546e7
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 22:57:45 +0200

    placeholder

--- compile.sh
+++ compile.sh
@@ -4,7 +4,10 @@ for entry in notes/*
 do 
   filename=$(basename "$entry" .md)
   timestamp=`git log --format=%ad --date=format:'%Y-%m-%d' -- $entry`
-  t+="{\"source\":\"$entry\",\"dest\":\"public/posts/$filename_$timestamp\",\"time\":\"$timestamp\"},"
+  dest="public/posts/$timestamp"
+  dest+="_$filename"
+  dest+=".html"
+  t+="{\"source\":\"$entry\",\"dest\":\"$dest\",\"time\":\"$timestamp\"},"
 done
 echo "${t%?}]"
 
