d09e689b1e6f49d6dd18504b9123a19ab79c28a8
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 22:49:06 +0200

    comma vergeten :)

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ for entry in notes/*
 do 
   filename=$(basename "$entry" .md)
   timestamp=`git log --format=%ad --date=format:'%Y-%m-%d' -- $entry`
-  t+="{\"source\":\"$entry\"\"dest\":\"public/posts/$filename_$timestamp\",\"time\":\"$timestamp\"},"
+  t+="{\"source\":\"$entry\",\"dest\":\"public/posts/$filename_$timestamp\",\"time\":\"$timestamp\"},"
 done
 echo "${t%?}]"
 
