f91f2b1c61e01d0c0169c9ed450df81c33774607
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 22:28:29 +0200

    forgot to remove debug statements

--- compile.sh
+++ compile.sh
@@ -2,13 +2,9 @@
 t='['
 for entry in notes/*
 do 
-  echo $entry
   filename=$(basename "$entry" .md)
-  echo $filename
   timestamp=`git log --format=%ad --date=format:'%Y-%m-%d' -- $entry`
-  echo $timestamp
   t+="{\"dest\"=\"$filename\",\"time\"=\"$timestamp\"},"
-  echo $t
 done
-echo "value=${t%?} ]"
+echo "value=${t%?}]"
 
