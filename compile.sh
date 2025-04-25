cde6c1ae96f97f0419374f6ddb9de6e0df8b11f5
bvanroll <beppe.vanrolleghem@gmail.com>
2025-04-25 22:41:07 +0200

    idk

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ for entry in notes/*
 do 
   filename=$(basename "$entry" .md)
   timestamp=`git log --format=%ad --date=format:'%Y-%m-%d' -- $entry`
-  t+="{\"dest\"=$filename,\"time\"=$timestamp},"
+  t+="{\"dest\"=\"$filename\",\"time\"=\"$timestamp\"},"
 done
 echo "${t%?}]"
 
